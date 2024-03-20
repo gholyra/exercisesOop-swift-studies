0da07f66c87d8925b0aa4eaa8c6c0529fc42d046
gholyra <tiago.mlyra@gmail.com>
2024-03-20 20:48:55 -0300

    Initial Commit

---
+++ POO - Sintaxe/Personagem.swift
@@ -0,0 +1,12 @@
+//
+//  Personagem.swift
+//  POO - Sintaxe
+//
+//  Created by Usuário Convidado on 20/03/24.
+//
+
+import Cocoa
+
+class Personagem: NSObject {
+
+}
---
+++ POO - Sintaxe/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  POO - Sintaxe
+//
+//  Created by Usuário Convidado on 20/03/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
